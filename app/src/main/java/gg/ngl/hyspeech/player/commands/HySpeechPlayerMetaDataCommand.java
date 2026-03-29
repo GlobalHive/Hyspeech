7143ec8dbcac155e1f4adaf7295ac25b45bb022b
GlobalHive <support@globalhive.ch>
2026-03-29 16:13:59 +0200

    Remove unused imports in HySpeechPlayerMetaDataCommand for cleaner code

--- app/src/main/java/gg/ngl/hyspeech/player/commands/HySpeechPlayerMetaDataCommand.java
+++ app/src/main/java/gg/ngl/hyspeech/player/commands/HySpeechPlayerMetaDataCommand.java
@@ -1,19 +1,12 @@
 package gg.ngl.hyspeech.player.commands;
 
-import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.arguments.system.RequiredArg;
 import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
-import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.universe.world.World;
-import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import gg.ngl.hyspeech.Hyspeech;
 import gg.ngl.hyspeech.player.HyspeechPlayer;
-import gg.ngl.hyspeech.player.ui.page.HyspeechDialogPage;
-
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 
